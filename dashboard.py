4539cf61d8d10fa2bf568d431caed4d7cf5482e6
JeremyUribe <108232545+JeremyUribe@users.noreply.github.com>
2026-01-18 19:14:47 -0500

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -160,4 +160,4 @@ import os
 
 port = int(os.environ.get("PORT", 10000))  # usa el puerto de Render, default 10000 local
 if __name__ == "__main__":
-    app.run_server(debug=True, host="0.0.0.0", port=port)
+    app.run(debug=True, host="0.0.0.0", port=port)
